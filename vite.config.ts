eb34a83182757254b9dc2f18458bc0446e62c8ec
Sean Crosby <seanmcrosby@gmail.com>
2026-03-07 21:26:01 -0700

    Set base path to /half-birthday/ for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/half-birthday/',
   plugins: [react()],
   test: {
     globals: true,
